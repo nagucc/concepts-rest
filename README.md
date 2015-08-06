fccb04abd97e22b6ec6929250d44a73ae357d414
Ynu AppEngine <yae@ynu.edu.cn>
2015-08-06 19:50:16 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# concepts-rest
-Concepts的REST接口
+# Concept管理api。
+v1.0.0
+目标：
+1. 实现Concept的创建、删除、获取
+2. 实现Concept的name和desc的增加、删除、获取
+3. 任何Concept的数据都将是完全公开的（相对于调用api的服务）。
--- app/controllers/concept.js
+++ app/controllers/concept.js
@@ -1,5 +1,10 @@
 /*
-微信企业号标签管理
+Concept管理api。
+v1.0.0
+目标：
+1. 实现Concept的创建、删除、获取
+2. 实现Concept的name和desc的增加、删除、获取
+3. 任何Concept的数据都将是完全公开的（相对于调用api的服务）。
 */
 
 var express = require('express'),
