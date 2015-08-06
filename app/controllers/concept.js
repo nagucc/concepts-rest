30990f3acfe3da01098e9faabe99190d4e996793
Ynu AppEngine <yae@ynu.edu.cn>
2015-08-06 19:45:31 +0800

    完成name和desc的增删

--- app/controllers/concept.js
+++ app/controllers/concept.js
@@ -12,6 +12,17 @@ module.exports = function (app, cfg) {
 };
 
 /*
+通用中间件，将执行结果以json的方式返回客户端。
+*/
+var sendResult = function(req, res, next){
+    if(res.err){
+        res.send({ret: -1, msg: res.err});
+    } else {
+        res.send({ret: 0, data: res.concept});
+    }
+};
+
+/*
 获取指定id的Concept
 */
 router.get('/:id', function(req, res, next){
@@ -19,13 +30,7 @@ router.get('/:id', function(req, res, next){
         _id: req.param.id
     };
     next();
-}, M.get, function(req, res, next){
-    if(res.err){
-        res.send({ret: -1, msg: res.err});
-    } else {
-        res.send({ret: 0, data: res.concept});
-    }
-});
+}, M.get, M.names, M.descs, sendResult);
 
 
 /*
@@ -37,11 +42,63 @@ router.put('/', M.create,
         req.concept.desc = req.body.desc;
         req.concept.data = {creator: req.body.creator};
         next();
-    }, M.addName, M.addDesc,
-    function(req, res, next) {
-        if(res.err){
-            res.send({ret: -1, msg: res.err});
-        } else {
-            res.send({ret: 0, data: res.concept});
+    }, M.addName, M.addDesc, sendResult);
+    
+/*
+为Concept添加name
+*/
+router.put('/:id/name', function(req, res, next){
+    req.concept = {
+        _id: req.param.id,
+        name: req.body.name,
+        data: {
+            creator: req.body.creator,
+            dateCreated: new Date()
         }
-    })
+    };
+    next();
+}, M.addName, sendResult);
+
+/*
+为Concept删除一个name
+*/
+router.delete('/:id/name', function(req, res, next){
+    req.concept = {
+        _id: req.param.id,
+        name: req.body.name,
+        data: {
+            creator: req.body.creator
+        }
+    }
+    next();
+}, M.removeName, sendResult);
+
+/*
+为Concept添加desc
+*/
+router.put('/:id/desc', function(req, res, next){
+    req.concept = {
+        _id: req.param.id,
+        desc: req.body.desc,
+        data: {
+            creator: req.body.creator,
+            dateCreated: new Date()
+        }
+    };
+    next();
+}, M.addDesc, sendResult);
+
+/*
+为Concept删除一个desc
+*/
+router.delete('/:id/desc', function(req, res, next){
+    req.concept = {
+        _id: req.param.id,
+        desc: req.body.desc,
+        data: {
+            creator: req.body.creator
+        }
+    }
+    next();
+}, M.removeDesc, sendResult);
+
