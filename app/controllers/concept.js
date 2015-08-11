5b1008950a27c024fefcfc33cfe74d5e2f0470e9
Ynu AppEngine <yae@ynu.edu.cn>
2015-08-11 15:31:28 +0800

    完成v1.0.0的测试

--- app/controllers/concept.js
+++ app/controllers/concept.js
@@ -10,100 +10,127 @@ v1.0.0
 var express = require('express'),
     router = express.Router();
 var config = require('../../config/config');
-var M = require('concepts').Middleware;
+var Middleware = require('nagu-concepts').Middleware;
 
 module.exports = function (app, cfg) {
-    app.use('/concept', router);
+    app.use('/concepts', router);
 };
 
+var M = new Middleware(config.neo_url);
 /*
 通用中间件，将执行结果以json的方式返回客户端。
 */
-var sendResult = function(req, res, next){
+var sendConcept = function(req, res, next){
     if(res.err){
         res.send({ret: -1, msg: res.err});
     } else {
-        res.send({ret: 0, data: res.concept});
+        res.send({ret: 0, data: {
+            id: res.concept.id,
+            labels: res.concept.labels,
+            properties: res.concept.properties,
+            names: res.concept.nameList || [],
+            descs: res.concept.descList || []
+        }});
     }
+};
+
+var prepareId = function(req, res, next){
+    req.concept = {
+        id: req.params.id
+    };
+    next();
+};
+
+var endIfError = function(req, res, next){
+    if(res.err) res.send({ret: -1, msg: res.err});
+    else next();
 };
 
 /*
 获取指定id的Concept
 */
-router.get('/:id', function(req, res, next){
+router.get('/:id', prepareId, M.get(), endIfError, M.names(), M.descs(), sendConcept);
+
+
+/*
+根据名称查找Concept
+*/
+router.get('/by-name/:name', function(req, res, next){
     req.concept = {
-        _id: req.param.id
+        name: req.params.name
     };
     next();
-}, M.get, M.names, M.descs, sendResult);
+}, M.findByName(), function(req, res, next){
+    if(res.err) res.send({ret: -1, msg: res.err});
+    else res.send({ ret: 0, data: res.conceptList});
+});
 
 
 /*
 创建Concept
 */
-router.put('/', M.create, 
+router.put('/', M.create(), 
     function(req, res, next){
         req.concept.name = req.body.name;
         req.concept.desc = req.body.desc;
         req.concept.data = {creator: req.body.creator};
         next();
-    }, M.addName, M.addDesc, sendResult);
+    }, M.addName(), M.addDesc(), sendConcept);
+
+
+/*
+删除指定id的Concept
+*/
+router.delete('/:id', prepareId, M.del(), function(req, res, next){
+    if(res.err){
+        res.send({ret: -1, msg: res.err});
+    } else {
+        res.send({ret: 0, data: {}});
+    }
+});
     
 /*
 为Concept添加name
 */
-router.put('/:id/name', function(req, res, next){
-    req.concept = {
-        _id: req.param.id,
-        name: req.body.name,
-        data: {
-            creator: req.body.creator,
-            dateCreated: new Date()
-        }
+router.put('/:id/name', prepareId, function(req, res, next){
+    req.concept.name = req.body.name;
+    req.concept.data = {
+        creator: req.body.creator,
+        dateCreated: new Date()
     };
     next();
-}, M.addName, sendResult);
+}, M.addName(), sendConcept);
 
 /*
 为Concept删除一个name
 */
-router.delete('/:id/name', function(req, res, next){
-    req.concept = {
-        _id: req.param.id,
-        name: req.body.name,
-        data: {
-            creator: req.body.creator
-        }
-    }
+router.delete('/:id/name', prepareId, function(req, res, next){
+    req.concept.name = req.body.name;
+    req.concept.data = {
+        creator: req.body.creator
+    };
     next();
-}, M.removeName, sendResult);
+}, M.removeName(), sendConcept);
 
 /*
 为Concept添加desc
 */
-router.put('/:id/desc', function(req, res, next){
-    req.concept = {
-        _id: req.param.id,
-        desc: req.body.desc,
-        data: {
-            creator: req.body.creator,
-            dateCreated: new Date()
-        }
+router.put('/:id/desc', prepareId, function(req, res, next){
+    req.concept.desc = req.body.desc;
+    req.concept.data = {
+        creator: req.body.creator,
+        dateCreated: new Date()
     };
     next();
-}, M.addDesc, sendResult);
+}, M.addDesc(), sendConcept);
 
 /*
 为Concept删除一个desc
 */
-router.delete('/:id/desc', function(req, res, next){
-    req.concept = {
-        _id: req.param.id,
-        desc: req.body.desc,
-        data: {
-            creator: req.body.creator
-        }
-    }
+router.delete('/:id/desc', prepareId, function(req, res, next){
+    req.concept.desc = req.body.desc,
+    req.concept.data = {
+        creator: req.body.creator
+    };
     next();
-}, M.removeDesc, sendResult);
-
+}, M.removeDesc(), sendConcept);
--- config/config.js
+++ config/config.js
@@ -5,12 +5,12 @@ var env = process.env.NODE_ENV || 'production';
 var config = {
   development: {
     port: 18080,
-    neo_url: 'http://localhost:7474'
+    neo_url: process.env.NEO_HOST || 'http://localhost:7474'
   },
 
   production: {
     port: 18080,
-    neo_url: process.env.NEO_HOST || 'http://localhost:7474'
+    neo_url: process.env.NEO_HOST || 'http://neo4j.ynu.edu.cn'
   }
 };
 
